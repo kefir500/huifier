3c300e35783e6844ca00dbda6930b531eed45d8d
Yuri B <6748193+YuriB9@users.noreply.github.com>
2022-11-11 06:34:41 +0300

    Add PowerShell implementation

---
+++ powershell/huifier.ps1
@@ -0,0 +1,34 @@
+﻿
+function Set-Huify ($Word) {
+
+    $Word = $Word.ToLower().Trim().ToCharArray()
+    $Vowels = 'аеёиоуыэюя'.ToCharArray()
+    $Rules = @{
+        'а' = 'я'
+        'о' = 'е'
+        'у' = 'ю'
+        'ы' = 'и'
+        'э' = 'е'
+    }
+
+    foreach ( $Letter in $Word ) {
+
+        if ( $Letter -in $Vowels ) {
+
+            if ($Letter -in $Rules.Keys) {
+                $Word = , $Rules["$Letter"] + $Word[1..$Word.Length]
+            }
+
+            break
+        }
+        else {
+            $Word = $Word[1..$Word.Length]
+        }
+
+    }
+
+    $Word ? 'Ху' + $($Word -join '') : 'Хуй'
+
+}
+
+Set-Huify -Word $(Read-Host "Введите слово")
